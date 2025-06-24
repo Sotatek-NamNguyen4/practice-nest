f2c8f1998d87b4158a09eb576cf6a34e4fae92c5
nam.nguyen4 <nam.nguyen4@sotatek.com>
2025-06-24 18:26:46 +0700

    fix: correct expired time token

--- src/modules/task/tasks.controller.ts
+++ src/modules/task/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, HttpCode, HttpStatus, Delete, Patch, Query, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpCode, HttpStatus, Delete, Patch, Query, Param, UseGuards } from '@nestjs/common';
 import { CreateTaskDto } from '../../dto/task/create-task.dto';
 import { TasksService } from '../task/tasks.service';
 import { UpdateTaskDto } from '../../dto/task/update-task.dto';
@@ -8,7 +8,6 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  @HttpCode(HttpStatus.OK)
   async getAllTasks(@Query('completed') completed?: string) {
     if (completed !== undefined) {
       const isCompleted = completed === 'true';
@@ -18,19 +17,16 @@ export class TasksController {
   }
 
   @Get(':id')
-  @HttpCode(HttpStatus.OK)
   async getTaskById(@Param('id') id: number) {
     return await this.tasksService.findOne(id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.CREATED)
   async createTask(@Body() body: CreateTaskDto) {
     return await this.tasksService.create(body);
   }
 
   @Patch(':id')
-  @HttpCode(HttpStatus.OK)
   async updateTask(@Param('id') id: number, @Body() body: UpdateTaskDto) {
     return await this.tasksService.update(id, body);
   }
